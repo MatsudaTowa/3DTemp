53a227f3ae6de48d6068b9f0cbe5864f822bb112
MatsudaTowa <231084@st.yoshida-g.ac.jp>
2024-06-18 11:44:38 +0900

    できた...のか？

--- プロジェクト/3D_Temp/objectX.cpp
+++ プロジェクト/3D_Temp/objectX.cpp
@@ -143,6 +143,9 @@ void CObjectX::Uninit()
 		m_pBuffMat->Release();
 		m_pBuffMat = nullptr;
 	}
+
+	Release();
+
 }
 
 //=============================================
@@ -157,56 +160,60 @@ void CObjectX::Update()
 //=============================================
 void CObjectX::Draw()
 {
-	//デバイスの取得
-	CRenderer* pRender = CManager::GetRenderer();
-	LPDIRECT3DDEVICE9 pDevice = pRender->GetDevice();
-	D3DMATERIAL9 matDef; //現在のマテリアルの保存
-	D3DXMATRIX mtxRot, mtxTrans; //計算用マトリックス
-
-//マトリックスの初期化
-	D3DXMatrixIdentity(&m_mtxWorld);
-
-	//αテストを有効
-	pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
-	pDevice->SetRenderState(D3DRS_ALPHAREF, 0);
-	pDevice->SetRenderState(D3DRS_ALPHAFUNC, D3DCMP_GREATER);
-
-	//向きを反映
-	D3DXMatrixRotationYawPitchRoll(&mtxRot, m_rot.y, m_rot.x, m_rot.z);
-
-	D3DXMatrixMultiply(&m_mtxWorld, &m_mtxWorld, &mtxRot);
-
-	//位置を反映
-	D3DXMatrixTranslation(&mtxTrans, m_pos.x, m_pos.y,m_pos.z);
-
-	D3DXMatrixMultiply(&m_mtxWorld, &m_mtxWorld, &mtxTrans);
-
-	//ワールドマトリックスの設定
-	pDevice->SetTransform(D3DTS_WORLD, &m_mtxWorld);
-
-	D3DXMATERIAL* pMat; //マテリアル
-
-	pMat = (D3DXMATERIAL*)m_pBuffMat->GetBufferPointer();
-
-	for (int nCntMat = 0; nCntMat < (int)m_dwNumMat; nCntMat++)
+	if (m_pMesh != nullptr && m_pBuffMat != nullptr)
 	{
-		//マテリアルの設定
-		pDevice->SetMaterial(&pMat[nCntMat].MatD3D);
+		//デバイスの取得
+		CRenderer* pRender = CManager::GetRenderer();
+		LPDIRECT3DDEVICE9 pDevice = pRender->GetDevice();
+		D3DMATERIAL9 matDef; //現在のマテリアルの保存
+		D3DXMATRIX mtxRot, mtxTrans; //計算用マトリックス
 
-		//テクスチャの設定
-		pDevice->SetTexture(0, m_pTexture[nCntMat]);
+	//マトリックスの初期化
+		D3DXMatrixIdentity(&m_mtxWorld);
 
-		//パーツの設定
-		m_pMesh->DrawSubset(nCntMat);
+		//αテストを有効
+		pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
+		pDevice->SetRenderState(D3DRS_ALPHAREF, 0);
+		pDevice->SetRenderState(D3DRS_ALPHAFUNC, D3DCMP_GREATER);
+
+		//向きを反映
+		D3DXMatrixRotationYawPitchRoll(&mtxRot, m_rot.y, m_rot.x, m_rot.z);
+
+		D3DXMatrixMultiply(&m_mtxWorld, &m_mtxWorld, &mtxRot);
+
+		//位置を反映
+		D3DXMatrixTranslation(&mtxTrans, m_pos.x, m_pos.y, m_pos.z);
+
+		D3DXMatrixMultiply(&m_mtxWorld, &m_mtxWorld, &mtxTrans);
+
+		//ワールドマトリックスの設定
+		pDevice->SetTransform(D3DTS_WORLD, &m_mtxWorld);
+
+		D3DXMATERIAL* pMat; //マテリアル
+
+		pMat = (D3DXMATERIAL*)m_pBuffMat->GetBufferPointer();
+
+		for (int nCntMat = 0; nCntMat < (int)m_dwNumMat; nCntMat++)
+		{
+			//マテリアルの設定
+			pDevice->SetMaterial(&pMat[nCntMat].MatD3D);
+
+			//テクスチャの設定
+			pDevice->SetTexture(0, m_pTexture[nCntMat]);
+
+			//パーツの設定
+			m_pMesh->DrawSubset(nCntMat);
+		}
+		//αテストを無効に
+		pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
+
+		//現在を取得
+		pDevice->GetMaterial(&matDef);
+
+		//保存してたマテリアルを戻す
+		pDevice->SetMaterial(&matDef);
 	}
-	//αテストを無効に
-	pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
-
-	//現在を取得
-	pDevice->GetMaterial(&matDef);
-
-	//保存してたマテリアルを戻す
-	pDevice->SetMaterial(&matDef);
+	
 }
 
 //=============================================
@@ -325,48 +332,52 @@ D3DXVECTOR3& CObjectX::GetRot()
 //=============================================
 D3DXVECTOR3& CObjectX::GetMinPos()
 {
-	int nNumVtx; //頂点数
-	DWORD sizeFVF; //頂点フォーマットのサイズ
-	BYTE* pVtxBuff; //頂点バッファのポインタ
-
-		//頂点数の取得
-	nNumVtx = m_pMesh->GetNumVertices();
-	//頂点フォーマットのサイズを取得
-	sizeFVF = D3DXGetFVFVertexSize(m_pMesh->GetFVF());
-
-	m_minpos = D3DXVECTOR3(100000.0f, 1000000.0f, 1000000.0f); //モデルの最小位置
-	m_maxpos = D3DXVECTOR3(-100000.0f, -1000000.0f, -100000.0f); //モデルの最大位置
-
-	//頂点バッファのロック
-	m_pMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVtxBuff);
-	for (int nCntVtx = 0; nCntVtx < nNumVtx; nCntVtx++)
+	if (m_pMesh != nullptr && m_pBuffMat != nullptr)
 	{
-		//頂点座標の代入
-		D3DXVECTOR3 vtx = *(D3DXVECTOR3*)pVtxBuff;
+		int nNumVtx; //頂点数
+		DWORD sizeFVF; //頂点フォーマットのサイズ
+		BYTE* pVtxBuff; //頂点バッファのポインタ
 
-		//x座標の最小値チェック
-		if (vtx.x < m_minpos.x)
+			//頂点数の取得
+		nNumVtx = m_pMesh->GetNumVertices();
+		//頂点フォーマットのサイズを取得
+		sizeFVF = D3DXGetFVFVertexSize(m_pMesh->GetFVF());
+
+		m_minpos = D3DXVECTOR3(100000.0f, 1000000.0f, 1000000.0f); //モデルの最小位置
+		m_maxpos = D3DXVECTOR3(-100000.0f, -1000000.0f, -100000.0f); //モデルの最大位置
+
+		//頂点バッファのロック
+		m_pMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVtxBuff);
+		for (int nCntVtx = 0; nCntVtx < nNumVtx; nCntVtx++)
 		{
-			m_minpos.x = vtx.x;
+			//頂点座標の代入
+			D3DXVECTOR3 vtx = *(D3DXVECTOR3*)pVtxBuff;
+
+			//x座標の最小値チェック
+			if (vtx.x < m_minpos.x)
+			{
+				m_minpos.x = vtx.x;
+			}
+
+			//y座標の最小値チェック
+			if (vtx.y < m_minpos.y)
+			{
+				m_minpos.y = vtx.y;
+			}
+
+			//z座標の最小値チェック
+			if (vtx.z < m_minpos.z)
+			{
+				m_minpos.z = vtx.z;
+			}
+
+			// 次の頂点に進む
+			pVtxBuff += sizeFVF;
 		}
 
-		//y座標の最小値チェック
-		if (vtx.y < m_minpos.y)
-		{
-			m_minpos.y = vtx.y;
-		}
-
-		//z座標の最小値チェック
-		if (vtx.z < m_minpos.z)
-		{
-			m_minpos.z = vtx.z;
-		}
-
-		// 次の頂点に進む
-		pVtxBuff += sizeFVF;
+		m_pMesh->UnlockVertexBuffer();
 	}
-
-	m_pMesh->UnlockVertexBuffer();
+	
 	return m_minpos;
 }
 
@@ -375,47 +386,51 @@ D3DXVECTOR3& CObjectX::GetMinPos()
 //=============================================
 D3DXVECTOR3& CObjectX::GetMaxPos()
 {
-	int nNumVtx; //頂点数
-	DWORD sizeFVF; //頂点フォーマットのサイズ
-	BYTE* pVtxBuff; //頂点バッファのポインタ
-
-		//頂点数の取得
-	nNumVtx = m_pMesh->GetNumVertices();
-	//頂点フォーマットのサイズを取得
-	sizeFVF = D3DXGetFVFVertexSize(m_pMesh->GetFVF());
-
-	m_minpos = D3DXVECTOR3(100000.0f, 1000000.0f, 1000000.0f); //モデルの最小位置
-	m_maxpos = D3DXVECTOR3(-100000.0f, -1000000.0f, -100000.0f); //モデルの最大位置
-
-//頂点バッファのロック
-	m_pMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVtxBuff);
-	for (int nCntVtx = 0; nCntVtx < nNumVtx; nCntVtx++)
+	if (m_pMesh != nullptr && m_pBuffMat != nullptr)
 	{
-		//頂点座標の代入
-		D3DXVECTOR3 vtx = *(D3DXVECTOR3*)pVtxBuff;
+		int nNumVtx; //頂点数
+		DWORD sizeFVF; //頂点フォーマットのサイズ
+		BYTE* pVtxBuff; //頂点バッファのポインタ
 
-		//x座標の最大値チェック
-		if (vtx.x > m_maxpos.x)
+			//頂点数の取得
+		nNumVtx = m_pMesh->GetNumVertices();
+		//頂点フォーマットのサイズを取得
+		sizeFVF = D3DXGetFVFVertexSize(m_pMesh->GetFVF());
+
+		m_minpos = D3DXVECTOR3(100000.0f, 1000000.0f, 1000000.0f); //モデルの最小位置
+		m_maxpos = D3DXVECTOR3(-100000.0f, -1000000.0f, -100000.0f); //モデルの最大位置
+
+	//頂点バッファのロック
+		m_pMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVtxBuff);
+		for (int nCntVtx = 0; nCntVtx < nNumVtx; nCntVtx++)
 		{
-			m_maxpos.x = vtx.x;
+			//頂点座標の代入
+			D3DXVECTOR3 vtx = *(D3DXVECTOR3*)pVtxBuff;
+
+			//x座標の最大値チェック
+			if (vtx.x > m_maxpos.x)
+			{
+				m_maxpos.x = vtx.x;
+			}
+
+			//y座標の最大値チェック
+			if (vtx.y > m_maxpos.y)
+			{
+				m_maxpos.y = vtx.y;
+			}
+
+			//z座標の最大値チェック
+			if (vtx.z > m_maxpos.z)
+			{
+				m_maxpos.z = vtx.z;
+			}
+			// 次の頂点に進む
+			pVtxBuff += sizeFVF;
 		}
 
-		//y座標の最大値チェック
-		if (vtx.y > m_maxpos.y)
-		{
-			m_maxpos.y = vtx.y;
-		}
-
-		//z座標の最大値チェック
-		if (vtx.z > m_maxpos.z)
-		{
-			m_maxpos.z = vtx.z;
-		}
-		// 次の頂点に進む
-		pVtxBuff += sizeFVF;
+		m_pMesh->UnlockVertexBuffer();
 	}
-
-	m_pMesh->UnlockVertexBuffer();
+	
 	return m_maxpos;
 }
 
